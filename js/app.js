5fc0e58eda78c8f98823aa1d9a10d091ce2620ac
Issa Al-aiash <izalayyash152@eng.just.edu.jo>
2021-03-01 16:39:27 +0200

    uploading the needed file for the about me game

---
+++ js/app.js
@@ -0,0 +1,47 @@
+'use strict';
+confirm('Lets see how well you know me\n answer these simple questions');
+let userName=prompt('What is your name dear');
+function greetings(name){
+  return alert('greetings ' + name +' hope you are well today');
+}
+greetings(userName);
+let birthGuess=prompt('Were I born in Zaqra').toLocaleLowerCase();
+if(birthGuess==='y' || birthGuess==='yes'){
+  alert('correct good job');
+  // console.log('correct good job')
+}else if(birthGuess==='n' || birthGuess==='no'){
+  alert('I thought you knew me better');
+  // console.log('I thought you knew me better');
+}
+let ageGuess=prompt('Am I 24 years old').toLocaleLowerCase();
+if(ageGuess==='y' || ageGuess==='yes'){
+  alert('correct good job');
+  // console.log('correct good job')
+}else if(ageGuess==='n' || ageGuess==='no'){
+  alert('I thought you knew me better');
+  // console.log('I thought you knew me better');
+}
+let heightGuess=prompt('Am I a short guy').toLocaleLowerCase();
+if(heightGuess==='y' || heightGuess==='yes'){
+  alert('correct good job');
+  // console.log('correct good job')
+}else if(heightGuess==='n' || heightGuess==='no'){
+  alert('I thought you knew me better');
+  // console.log('I thought you knew me better');
+}
+let smartGuess=prompt('Am I smart').toLocaleLowerCase();
+if(smartGuess==='y' || smartGuess==='yes'){
+  alert('correct good job');
+  // console.log('correct good job')
+}else if(smartGuess==='n' || smartGuess==='no'){
+  alert('rude !!!!');
+  // console.log('rude !!!!');
+}
+let physicsGuess=prompt('Am I into quantum phyics').toLocaleLowerCase();
+if(physicsGuess==='y' || physicsGuess==='yes'){
+  alert('correct good job ' +userName);
+  // console.log('correct good job ' + userName)
+}else if(smartGuess==='n' || smartGuess==='no'){
+  alert('wrong I love it '+ userName);
+  // console.log('wrong I love it ' + userName);
+}
